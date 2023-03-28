0f3c26ec8a3d0d2367f9ba2d3df56e3b70e78eec
MarcoTuc <marco.tuccio95@gmail.com>
2023-03-28 17:06:15 +0200

    2dmerge

--- 2Drun.py
+++ 2Drun.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 from scipy.optimize import dual_annealing
 
-EXPNAME = 'twodimensionalPORCODIO'
+EXPNAME = 'twodimensional'
 
 # Import experimental data from Hertel 
 expdata = pd.read_csv('./data/herteldata.csv', names=['sequences', 'experimental'])
